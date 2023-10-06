0eb81c50f010fa6450a8a315e12dd5a3e81c7af2
Koray Dundar <koray.dundar@exove.com>
2023-10-06 10:48:58 +0300

    completed normalization and product details page

--- framework/common/types/product.ts
+++ framework/common/types/product.ts
@@ -1,21 +1,38 @@
-import { ProductPriceRange } from "@framework/schema"
-
 export interface ProductImage {
     url: string
     alt?: string
-}
+  }
 
-export interface ProductPrice {
+  export interface ProductPrice {
     value: number
     currencyCode: "USD" | "EUR" | string
-}
+  }
 
-export interface Product {
+  export interface ProductOptionValues {
+    label: string
+    hexColor?: string
+  }
+
+  export interface ProductOption {
+    id: string
+    displayName: string
+    values: ProductOptionValues[]
+  }
+
+  export interface ProductVariant {
+    id: string
+    name: string
+    options: ProductOption[]
+  }
+
+  export interface Product {
     id: string
     name: string
     description: string
     slug: string
     path: string
     images: ProductImage[]
-    price: ProductPrice
-}
+    price: ProductPrice,
+    options: ProductOption[],
+    variants: ProductVariant[]
+  }
